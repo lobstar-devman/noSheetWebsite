887799e295e5964f42042b501828833773dee8ab
devops-at-alinea <devops-at-alinea@users.noreply.github.com>
2025-07-23 14:03:09 +0100

    removed un wanted files

--- docs/noSheetLibrary/README.md
+++
@@ -1,5 +0,0 @@
-
-# NoSheet
-A JavaScript alternative to HSL's (Horrible Spreadsheet Libraries)
-
-{{<include "/nosheet_source/README.md" >}}
--- docs/noSheetLibrary/examples/js/examples/facets/raw_materials.mjs
+++
@@ -1,8 +0,0 @@
-"use strict";
-/**
-Calculate line item cost from raw materials
-*/
-export default function(table, row, refs) {
-
-    row.unit_cost = () => row.steel_m2 * (refs.manufacturing_cost_per_m2 + refs.average_steel_cost_m2);
-};
